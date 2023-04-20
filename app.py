e93b09ce97e5c2b70729a83368e99bf913d75b72
G.Hemanth Sai <73033596+HemanthSai7@users.noreply.github.com>
2023-04-20 12:16:39 +0530

    Updated the repository structure compatible with fastAPI
    Added modules to handle the connection,model loading, env loading etc

--- app.py
+++ app.py
@@ -1,8 +1,1 @@
-from PhishnetAPI.services.Address import AddressBar as addr
-import time
-
-print(addr.get_url())
-addr.set_url("https://www.google.com")
-time.sleep(5)
-
-addr.redirect_url("https://www.google.com")
+from PhishnetAPI import app
